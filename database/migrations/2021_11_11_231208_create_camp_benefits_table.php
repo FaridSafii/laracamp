25af0ae779a2ef642333960e2ffdcbb3a67dd738
FaridSafii <mohammedfaridalisafii@gmail.com>
2021-11-12 09:32:55 +0800

    Finish migration

---
+++ database/migrations/2021_11_11_231208_create_camp_benefits_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCampBenefitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('camp_benefits', function (Blueprint $table) {
+            $table->id();
+            //1st Method
+            //$table->bigInteger('camp_id')->unsigned();
+            //bisa juga seperti ini
+            //$table->unsignedBigInteger('camp_id');
+            
+
+            //2nd method
+            //Syaratnya harus sama nama table dan field yang digunakan : camp -> table , id -> field primary
+            $table->foreignId('camp_id')->constrained();
+
+
+            $table->string('name');
+            $table->timestamps();
+            //1st method
+            //$table->foreign('camp_id')->references('id')->on('camps')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('camp_benefits');
+    }
+}

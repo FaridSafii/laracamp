29f0f72ed98b4db202bc15a34ee3c21b4f8fe854
FaridSafii <mohammedfaridalisafii@gmail.com>
2021-11-18 22:49:25 +0800

    Middleware dashboard, inetgration admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,61 +1,59 @@
 @extends('layout.app')
 @section('content')
-<section class="dashboard my-5">
-        <div class="container">
-            <div class="row text-left">
-                <div class=" col-lg-12 col-12 header-wrap mt-4">
-                    <p class="story">
-                        DASHBOARD
-                    </p>
-                    <h2 class="primary-header ">
-                        My Bootcamps
-                    </h2>
-                </div>
+<section>
+<div class="container">
+    <div class="row">
+        <div class="col-8 offset-2">
+            <div class="card-header">
+                My Camps
             </div>
-            <div class="row my-5">
+            <div class="card-body">
                 @include('component.alert')
-                <table class="table">
+                <table class="table" style="">
+                    <thead>
+                        <tr>
+                            <th>User</th>
+                            <th>Camp</th>
+                            <th>Price</th>
+                            <th>Register Data</th>
+                            <th>Paid Status</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
                     <tbody>
                         @forelse ($checkouts as $checkout)
-                        <tr class="align-middle">
-                            <td width="18%">
-                                <img src="{{ asset('assets/images/item_bootcamp.png') }}" height="120" alt="">
-                            </td>
-                            <td>
-                                <p class="mb-2">
-                                    <strong>{{ $checkout->Camp->title }}</strong>
-                                </p>
-                                <p>
-                                    {{ $checkout->created_at->format('M d, Y') }}
-                                </p>
-                            </td>
-                            <td>
-                                <strong>${{ $checkout->Camp->price }}k</strong>
-                            </td>
-                            <td>
-                                @if ($checkout->is_paid)
-                                <strong class="text-success">Payment Success</strong>
-                                @else
-                                <strong>Waiting for Payment</strong>
-                                @endif
-                                
-                            </td>
-                            <td>
-                                <a href="https://wa.me/082313090692?text=Hi, Saya ingin bertanya tentang kelas {{ $checkout->Camp->title }}" class="btn btn-primary">
-                                    Contact Support
-                                </a>
-                            </td>
-                        </tr>  
+                            <tr>
+                                <td>{{ $checkout->User->name }}</td>
+                                <td>{{ $checkout->Camp->title }}</td>
+                                <td>{{ $checkout->Camp->price }}</td>
+                                <td>{{ $checkout->created_at->format('M d Y') }}</td>
+                                <td>
+                                    @if ($checkout->is_paid)
+                                        <span class="badge bg-success">Paid</span>
+                                    @else
+                                        <span class="badge bg-warning">Waiting</span>
+                                    @endif
+                                </td>
+                                <td>
+                                    @if (!$checkout->is_paid)
+                                    <form action="{{ route('admin.checkout.update',$checkout->id) }}" method="post">
+                                        @csrf
+                                        <button class="btn btn-primary btn-sm">Set to Paid </button>
+                                    </form>  
+                                    @endif
+                                   
+                                </td>
+                            </tr>
                         @empty
-                            <tr colspan="5">
-                                No Data
+                            <tr>
+                               <td colspan="3"> No camps regitered</td> 
                             </tr>
                         @endforelse
-
-
                     </tbody>
                 </table>
             </div>
         </div>
-    </section>
+    </div>
+</div>
+</section>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\User\CheckoutController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\User\DashboardController as UserDashboard;
 use App\Http\Controllers\Admin\DashboardController as AdminDashboard;
+use App\Http\Controllers\Admin\CheckoutController as AdminCheckout;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,21 +29,22 @@ Route::get('sign-in-google',[UserController::class,'google'])->name('user.login.
 Route::get('auth/google/callback',[UserController::class,'handleProviderCallback'])->name('user.login.callback');
 
 Route::middleware(['auth'])->group(function(){
-    Route::get('checkout/success', [CheckoutController::class,'success'])->name('checkout.success');
-    Route::get('checkout/{camp:slug}', [CheckoutController::class,'create'])->name('checkout');
-    Route::post('checkout/{camp}', [CheckoutController::class,'store'])->name('checkout.store');
+    Route::get('checkout/success', [CheckoutController::class,'success'])->name('checkout.success')->middleware('ensureUserRole:user');
+    Route::get('checkout/{camp:slug}', [CheckoutController::class,'create'])->name('checkout')->middleware('ensureUserRole:user');
+    Route::post('checkout/{camp}', [CheckoutController::class,'store'])->name('checkout.store')->middleware('ensureUserRole:user');
     
     //dashboard
     Route::get('dashboard',[HomeController::class,'dashboard'])->name('dashboard');
 
     //user dashboard
-    Route::prefix('user/dashboard')->namespace('User')->name('user.')->group(function(){
+    Route::prefix('user/dashboard')->namespace('User')->name('user.')->middleware('ensureUserRole:user')->group(function(){
         Route::get('/',[UserDashboard::class,'index'])->name('dashboard');
     });
 
     //admin dashboard
-    Route::prefix('admin/dashboard')->namespace('Admin')->name('admin.')->group(function(){
+    Route::prefix('admin/dashboard')->namespace('Admin')->name('admin.')->middleware('ensureUserRole:admin')->group(function(){
         Route::get('/',[AdminDashboard::class,'index'])->name('dashboard');
+        Route::post('checkout/{checkout}',[AdminCheckout::class,'update'])->name('checkout.update');
     });
 
     //Route::get('dashboard/checkout/invoice/{checkout}',[CheckoutController::class,'invoice'])->name('user.checkout.invoice');
